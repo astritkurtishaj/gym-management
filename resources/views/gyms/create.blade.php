2fa91627cc7c15c7a727ed9d219979e9ee7b224e
Astrit <astritkurtishaj@gmail.com>
2021-04-05 00:01:57 +0200

    UI improvements

--- resources/views/gyms/create.blade.php
+++ resources/views/gyms/create.blade.php
@@ -1,31 +1,35 @@
 @extends('layouts.app')
 @section('content')
 
-    <h1>Formulari per Regjistirmin e Antareve te ri</h1>
-    <form class="col-6 m-auto" method="POST" action="{{ url('members') }}" enctype="multipart/form-data">
+    <br>
+    <div class="container m-auto col-5">
+        <h3>Add Gym Member Form</h3>
+    </div><br>
+
+    <form class="col-5 m-auto" method="POST" action="{{ url('members') }}" enctype="multipart/form-data">
         @csrf
         <div class="mb-3">
-            <label for="first_name" class="form-label">Emri</label>
+            <label for="first_name" class="form-label">First Name</label>
             <input type="text" class="form-control" id="first_name" name="first_name" required>
         </div>
         <div class="mb-3">
-            <label for="last_name" class="form-label">Mbiemri</label>
+            <label for="last_name" class="form-label">Last Name</label>
             <input type="text" class="form-control" id="last_name" name="last_name" required>
         </div>
         <div class="mb-3">
-            <label for="birthdate" class="form-label">Data Lindjes</label>
+            <label for="birthdate" class="form-label">Birthdate</label>
             <input type="date" class="form-control" id="birthdate"  name="birthdate" required>
         </div>
         <div class="mb-3">
-            <label for="expire_date" class="form-label">Data Skadimit</label>
+            <label for="expire_date" class="form-label">Expire Date</label>
             <input type="date" class="form-control" id="expire_date" name="expire_date" required>
         </div>
 
         <div class="mb-3">
-            <label for="profile_picture" class="form-label">Foto</label>
+            <label for="profile_picture" class="form-label">Profile Picture</label>
             <input type="file" class="form-control" id="profile_picture" name="profile_picture">
         </div>
 
-        <button class="btn btn-primary" type="submit">Ruaj</button>
+        <button class="btn btn-primary" type="submit">Save</button>
     </form>
 @stop
--- resources/views/gyms/edit.blade.php
+++ resources/views/gyms/edit.blade.php
@@ -1,32 +1,34 @@
 @extends('layouts.app')
 @section('content')
 
-    <h1>Formulari per Editimin e Antarit</h1>
+    <br>
+    <div class="container m-auto col-5">
+        <h3>Edit Gym Member Form</h3>
+    </div><br>
     <form class="col-6 m-auto" method="POST" action="{{ url("members/$member->id") }}" enctype="multipart/form-data">
         @csrf
         {{ method_field('PATCH') }}
         <div class="mb-3">
-            <label for="first_name" class="form-label">Emri</label>
+            <label for="first_name" class="form-label">First Name</label>
             <input type="text" class="form-control" id="first_name" name="first_name" value="{{ $member->first_name }}" required>
         </div>
         <div class="mb-3">
-            <label for="last_name" class="form-label">Mbiemri</label>
+            <label for="last_name" class="form-label">Last Name</label>
             <input type="text" class="form-control" id="last_name" name="last_name" value="{{ $member->last_name }}" required>
         </div>
         <div class="mb-3">
-            <label for="birthdate" class="form-label">Data Lindjes</label>
+            <label for="birthdate" class="form-label">Birthdate</label>
             <input type="date" class="form-control" id="birthdate"  name="birthdate" value="{{ $member->birthdate->toDateString() }}" required>
         </div>
         <div class="mb-3">
-            <label for="expire_date" class="form-label">Data Skadimit</label>
+            <label for="expire_date" class="form-label">Expire Date</label>
             <input type="date" class="form-control" id="expire_date" name="expire_date" value="{{ $member->expire_date->toDateString() }}" required>
         </div>
 
         <div class="mb-3">
-            <label for="profile_picture" class="form-label">Foto</label>
+            <label for="profile_picture" class="form-label">Profile Picture</label>
             <input type="file" class="form-control" id="profile_picture" name="profile_picture">
         </div>
-
-        <button class="btn btn-primary" type="submit">Ruaj</button>
+        <button class="btn btn-primary" type="submit">Save</button>
     </form>
 @stop
